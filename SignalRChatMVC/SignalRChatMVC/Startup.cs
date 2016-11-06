b7cffc5ef4e0aebfdb2d27744aa686c3cd27e6d2
Locust1 <bartlomiej.tyla@gmail.com>
2016-11-06 17:16:33 +0100

    Implementation of SignalRChatMVC

---
+++ SignalRChatMVC/SignalRChatMVC/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SignalRChatMVC.Startup))]
+
+namespace SignalRChatMVC
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
